26b32294c118ff8f01346e947ebdfa2edfecb000
A-str0 <il.eroxin@gmail.com>
2025-09-18 23:58:41 +0300

    feature/backend/users: implement very basic User database model in `users/models.py`

--- backend/django_project/apps/users/models.py
+++ backend/django_project/apps/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    """
+    Basic User model
+    """
+
+    email = models.EmailField(verbose_name='email address', unique=True)
+    points = models.IntegerField(verbose_name='points', default=0)
+
+    # TODO: UsersGroup Foreign Key
+    # users_group = models.ForeignKey()
