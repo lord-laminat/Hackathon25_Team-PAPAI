60334e3bdc3b0d79cc757d5e0262580333a29cbb
A-str0 <il.eroxin@gmail.com>
2025-09-19 16:53:54 +0300

    feature/backend/users: renamed `User` model to `CustomUser`

--- backend/django_project/apps/users/admin.py
+++ backend/django_project/apps/users/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import CustomUser
 
-@admin.register(User)
-class UserAdmin(UserAdmin):
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
     pass
--- backend/django_project/apps/users/models.py
+++ backend/django_project/apps/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class User(AbstractUser):
+class CustomUser(AbstractUser):
     """
     Basic User model
     """
