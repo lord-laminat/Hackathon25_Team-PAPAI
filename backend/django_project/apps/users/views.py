01d03588984bc6e09234a54973f4257411fa8ca4
A-str0 <il.eroxin@gmail.com>
2025-09-19 16:53:01 +0300

    feature/backend/users: create basic `login` view

--- backend/django_project/apps/users/views.py
+++ backend/django_project/apps/users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+# from django.template.loader import get_template
+from django.http import HttpResponse
 
 # Create your views here.
+def login(request):
+    # template = get_template("")
+    return HttpResponse(render(request=request, template_name="login.html"))
