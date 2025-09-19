c57cb56b11360623b6ed63e3c3005bdc0527be9c
A-str0 <il.eroxin@gmail.com>
2025-09-20 00:29:55 +0300

    feaure/backend/users: fix `CustomUser` model creation bug

--- backend/django_project/apps/users/models.py
+++ backend/django_project/apps/users/models.py
@@ -18,14 +18,14 @@ class CustomUser(AbstractUser):
     """
 
     email = models.EmailField(verbose_name='email address', unique=True)
-    skills = models.JSONField(verbose_name='skills', default={})
+    skills = models.JSONField(verbose_name='skills', default=dict())
     experience_points = models.PositiveIntegerField(verbose_name='experience_points', default=0)
     mana_points = models.PositiveIntegerField(verbose_name='mana_points', default=0)
     rank = models.PositiveSmallIntegerField(verbose_name='rank', choices=Rank.choices(), default=Rank.BEGINNER.value)
 
 
     def __str__(self):
-        return f"User: {self.get_full_name()} - {self.email} ({self.points} points)"
+        return f"User: {self.get_full_name()} - {self.email} | {self.experience_points} points ({self.rank} rank)"
 
 
     class Meta:
