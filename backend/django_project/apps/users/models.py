c16c0f584118033de67386cb3bec9a437a7d5eec
A-str0 <il.eroxin@gmail.com>
2025-09-19 21:21:39 +0300

    feature/backend/users: update `CustomUser` model and make migrations

--- backend/django_project/apps/users/models.py
+++ backend/django_project/apps/users/models.py
@@ -1,19 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from .enums import Rank
 
-# Create your models here.
+
 class CustomUser(AbstractUser):
     """
-    Basic User model
+    Basic User proxy model
     """
-
     email = models.EmailField(verbose_name='email address', unique=True)
-    points = models.IntegerField(verbose_name='points', default=0)
 
-    # TODO: UsersGroup Foreign Key
-    # users_group = models.ForeignKey()
+    skills = models.JSONField(verbose_name='skills', default={})
+    experience_points = models.IntegerField(verbose_name='experience_points', default=0)
+    mana_points = models.PositiveIntegerField(verbose_name='mana_points', default=0)
+    rank = models.PositiveSmallIntegerField(verbose_name='rank', choices=Rank.choices(), default=Rank.BEGINNER.value)
 
-    
+
     def __str__(self):
         return f"User: {self.get_full_name()} - {self.email} ({self.points} points)"
 
@@ -21,4 +22,8 @@ class CustomUser(AbstractUser):
     class Meta:
         default_related_name = 'custom_users'
 
-        permissions = [] # TODO: add permisions
+        permissions = [
+            # ('see_rank_beginner', 'Can see tasks for Beginner rank')
+            # ('see_rank_intermediate', 'Can see tasks for Intermediate rank')
+            # ('see_rank_advanced', 'Can see tasks for Advanced rank')
+        ] # TODO: add permissions
