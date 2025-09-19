36fa4b1eab1606bcd11d13b5b5160cfcb86e1859
A-str0 <il.eroxin@gmail.com>
2025-09-19 11:46:36 +0300

    feature/backend/users: include `users` app in `django_project`

--- backend/django_project/django_project/settings.py
+++ backend/django_project/django_project/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_project.users.UsersConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
