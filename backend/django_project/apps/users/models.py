29ff8b9ac916cbc07ca336d4fee47d2bc1e415f8
A-str0 <il.eroxin@gmail.com>
2025-09-20 00:19:30 +0300

    feature/backend/users: add docstring for `CustomUser`

--- backend/django_project/apps/users/models.py
+++ backend/django_project/apps/users/models.py
@@ -5,12 +5,21 @@ from .enums import Rank
 
 class CustomUser(AbstractUser):
     """
-    Basic User proxy model
-    """
-    email = models.EmailField(verbose_name='email address', unique=True)
+    Basic implementation of **CustomUser** model for *Django ORM*.
+    It inherits from `django.contrib.auth.models.AbstractUser`
+    and using all fields from it.
 
+    Args:
+        email: Unique `EmailField` 
+        skills: `JSONField` that contains json object describing the user's skills
+        experience_points: `PositiveIntegerField` that contains XP
+        mana_points: `PositiveIntegerField` that contains mana points for exchanging onto merch and other awards
+        rank: `PositiveSmallIntegerField` created via Rank enum in `enums.py` and describes current user's rank
+    """
+
+    email = models.EmailField(verbose_name='email address', unique=True)
     skills = models.JSONField(verbose_name='skills', default={})
-    experience_points = models.IntegerField(verbose_name='experience_points', default=0)
+    experience_points = models.PositiveIntegerField(verbose_name='experience_points', default=0)
     mana_points = models.PositiveIntegerField(verbose_name='mana_points', default=0)
     rank = models.PositiveSmallIntegerField(verbose_name='rank', choices=Rank.choices(), default=Rank.BEGINNER.value)
 
