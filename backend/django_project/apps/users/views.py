aee4a4de038e2ddbc33d47eafd253d2fc8c68ad0
A-str0 <il.eroxin@gmail.com>
2025-09-19 18:19:36 +0300

    feat/backend/users: implement basic user authentication form

--- backend/django_project/apps/users/views.py
+++ backend/django_project/apps/users/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
-# from django.template.loader import get_template
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
-# Create your views here.
 def login(request):
-    # template = get_template("")
-    return HttpResponse(render(request=request, template_name="login.html"))
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        print("Yes")
+
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        print("No")
+        form = LoginForm()
+
+    return HttpResponse(render(request=request, template_name="login.html", context={'form' : form}))
--- backend/django_project/django_project/settings.py
+++ backend/django_project/django_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'apps/users/templates'],
+        'DIRS': [BASE_DIR / 'apps/users/templates', BASE_DIR / 'django_project/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
